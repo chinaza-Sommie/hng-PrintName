aeec8b6e6f321902aba66d4fe9151c3ce74e98e0
chinaza-Sommie <85754500+chinaza-Sommie@users.noreply.github.com>
2021-08-17 23:20:36 -0700

    Add files via upload

---
+++ chinaza_code.php
@@ -0,0 +1,11 @@
+<?php
+
+	$firstname = "Chinaza";
+	$middlename = "Chisom";
+	$Lastname   = "Chukwunweike";
+
+
+	$Fullname = $firstname. " ".$middlename. " ".$Lastname;
+
+	echo "<h1 style='margin:50px'>".$Fullname ."</h1";
+?>
